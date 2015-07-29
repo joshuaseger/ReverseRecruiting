8c6fdd4bcc764824eb967abcd5803d984fe04d04
Joshua Seger <joshuaseger@loaner65m.agilesports.local>
2015-07-29 14:41:27 -0500

    Get back on sync.

--- map-filters.js
+++ map-filters.js
@@ -1,24 +1,39 @@
-var filtered;
+var filtered = [];
+//UpdateColleges();
+
+function UpdateColleges() {
+	$("#collegeOutput").empty();
+	$("#collegeOutput").append("<th>Schools</th>");
+	for (i=0; i < colleges.length; i++){
+		var name = colleges[i].schoolName;
+		name = name.split("(")[0];
+		$("#collegeOutput").append("<tr><td>"+name+"</td></tr>");
+    }
+}
 
 function filterByState() {
+	colleges = [];
     var map = $('#map').vectorMap('get', 'mapObject');
-    var regions = map.getRegionName();
+    var regions = map.getSelectedRegions();
+    console.log(regions);
     
     if (regions.length > 0) {
         for (i=0; i < data.length; i++) {
-            for (j=0; i < regions.length; j++) {
-                console.log("data state: " + data[i].state + " | map state: " + regions[j].getRegionName());
-                if(data[i].state = regions[j].getRegionName()) {
+            for (j=0; j < regions.length; j++) {
+                console.log("data state: " + data[i].state + " | map state: " + map.getRegionName(regions[j]));
+                if(data[i].state == map.getRegionName(regions[j])) {
+                	console.log("PUSHING " + data[i].schoolName + " into filtered array ---------------------");
                     filtered.push(data[i]);
                 }
             }
         }
     }
-    if (filtered.length != 0) {
+    if (filtered.length >= 0) {
         colleges = filtered;
     } else {
         colleges = data;
     }
+
 }
 // recalculate filtered
 
