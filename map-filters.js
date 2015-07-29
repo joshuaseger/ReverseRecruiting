7aed222cebed4b44d205f4601177da8ecde72aca
Austin Wendt <austin.wendt@hudl.com>
2015-07-29 12:21:58 -0600

    State filtering

--- map-filters.js
+++ map-filters.js
@@ -1,1 +1,24 @@
-var colleges = data;
+var filtered;
+//get values of filters
+var map = $('#map').vectorMap('get', 'mapObject');
+var regions = map.getSelectedRegions();
+
+function filterByState() {
+    if (regions.length > 0) {
+        for (i=0; i < data.length; i++) {
+            for (j=0; i < regions.length; j++) {
+                if(data[i].state = regions[j].getRegionName()) {
+                    filtered.push(data[i]);
+                }
+            }
+        }
+    }
+    if (filtered.length != 0) {
+        colleges = filtered;
+    } else {
+        colleges = data;
+    }
+}
+// recalculate filtered
+
+
