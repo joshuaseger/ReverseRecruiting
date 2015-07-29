ba04ef50785259549931efbf25d2222ab5b53fd5
Austin Wendt <austin.wendt@hudl.com>
2015-07-29 12:56:33 -0600

    Added filterning

--- map-filters.js
+++ map-filters.js
@@ -1,12 +1,13 @@
 var filtered;
-//get values of filters
-var map = $('#map').vectorMap('get', 'mapObject');
-var regions = map.getSelectedRegions();
 
 function filterByState() {
+    var map = $('#map').vectorMap('get', 'mapObject');
+    var regions = map.getRegionName();
+    
     if (regions.length > 0) {
         for (i=0; i < data.length; i++) {
             for (j=0; i < regions.length; j++) {
+                console.log("data state: " + data[i].state + " | map state: " + regions[j].getRegionName());
                 if(data[i].state = regions[j].getRegionName()) {
                     filtered.push(data[i]);
                 }
