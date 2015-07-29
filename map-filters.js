bf4de5af267329ad6815585a9049eabafb8100de
Joshua Seger <joshuaseger@loaner65m.agilesports.local>
2015-07-29 15:14:58 -0500

    WORKING state filter

--- map-filters.js
+++ map-filters.js
@@ -1,6 +1,3 @@
-var filtered = [];
-//UpdateColleges();
-
 function UpdateColleges() {
 	$("#collegeOutput").empty();
 	$("#collegeOutput").append("<th>Schools</th>");
@@ -10,16 +7,22 @@ function UpdateColleges() {
 		$("#collegeOutput").append("<tr><td>"+name+"</td></tr>");
     }
 }
-
 function filterByState() {
+	console.log(data.length);
 	colleges = [];
+	console.log(data.length);
+	var filtered = [];
     var map = $('#map').vectorMap('get', 'mapObject');
     var regions = map.getSelectedRegions();
-    console.log(regions);
+    console.log(regions.length);
     
+    console.log(data.length);
     if (regions.length > 0) {
+    	console.log(data.length);
         for (i=0; i < data.length; i++) {
+        	console.log("hi");
             for (j=0; j < regions.length; j++) {
+
                 console.log("data state: " + data[i].state + " | map state: " + map.getRegionName(regions[j]));
                 if(data[i].state == map.getRegionName(regions[j])) {
                 	console.log("PUSHING " + data[i].schoolName + " into filtered array ---------------------");
@@ -33,7 +36,7 @@ function filterByState() {
     } else {
         colleges = data;
     }
-
+    UpdateColleges();
 }
 // recalculate filtered
 
