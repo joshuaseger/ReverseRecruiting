29679ee23b84187f10fefe9f1814fc2759f8394b
Joshua Seger <joshuaseger@loaner65m.agilesports.local>
2015-07-29 15:34:18 -0500

    Remove fluff

--- map-filters.js
+++ map-filters.js
@@ -1,6 +1,5 @@
 function UpdateColleges() {
 	$("#collegeOutput").empty();
-	$("#collegeOutput").append("<th>Schools</th>");
 	for (i=0; i < colleges.length; i++){
 		var name = colleges[i].schoolName;
 		name = name.split("(")[0];
@@ -14,15 +13,11 @@ function filterByState() {
 	var filtered = [];
     var map = $('#map').vectorMap('get', 'mapObject');
     var regions = map.getSelectedRegions();
-    console.log(regions.length);
-    
-    console.log(data.length);
+
     if (regions.length > 0) {
     	console.log(data.length);
         for (i=0; i < data.length; i++) {
-        	console.log("hi");
             for (j=0; j < regions.length; j++) {
-
                 console.log("data state: " + data[i].state + " | map state: " + map.getRegionName(regions[j]));
                 if(data[i].state == map.getRegionName(regions[j])) {
                 	console.log("PUSHING " + data[i].schoolName + " into filtered array ---------------------");
