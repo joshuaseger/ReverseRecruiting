2e38f6b40da067e4fb82d8c739eb75d5b717cd54
Austin Wendt <austin.wendt@hudl.com>
2015-07-29 14:40:05 -0600

    Fixed a filtering bug

--- map-filters.js
+++ map-filters.js
@@ -8,32 +8,24 @@ function UpdateColleges() {
     }
 }
 function filterByState() {
-	console.log(data.length);
 	colleges = [];
-	console.log(data.length);
 	var filtered = [];
     var map = $('#map').vectorMap('get', 'mapObject');
     var regions = map.getSelectedRegions();
-    console.log(regions.length);
     
-    console.log(data.length);
     if (regions.length > 0) {
-    	console.log(data.length);
         for (i=0; i < data.length; i++) {
-        	console.log("hi");
             for (j=0; j < regions.length; j++) {
-
-                console.log("data state: " + data[i].state + " | map state: " + map.getRegionName(regions[j]));
                 if(data[i].state == map.getRegionName(regions[j])) {
-                	console.log("PUSHING " + data[i].schoolName + " into filtered array ---------------------");
                     filtered.push(data[i]);
                 }
             }
         }
     }
-    if (filtered.length > 0) {
+   
+    if (filtered.length >= 0 && regions.length >= 1) {
         colleges = filtered;
-    } else {
+    } else {  
         colleges = data;
     }
     UpdateColleges();
